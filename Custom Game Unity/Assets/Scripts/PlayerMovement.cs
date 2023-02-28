8c1c2cef3419550d4c38ec893878390808a89faf
NKT-FOR-CODE <116135390+NKT-FOR-CODE@users.noreply.github.com>
2023-02-28 10:11:34 -0800

    start of playermovement

--- Custom Game Unity/Assets/Scripts/PlayerMovement.cs
+++ Custom Game Unity/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform camera;
+    public float speed = 2;
+
+
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("space");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("A");
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            
+        }
     }
 }
