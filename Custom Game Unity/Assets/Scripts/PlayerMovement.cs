7828e4b6b1e6f5f4fe56d0608a9d67d00b2d67a6
NKT-FOR-CODE <116135390+NKT-FOR-CODE@users.noreply.github.com>
2023-03-03 09:58:07 -0800

    updated player movement

--- Custom Game Unity/Assets/Scripts/PlayerMovement.cs
+++ Custom Game Unity/Assets/Scripts/PlayerMovement.cs
@@ -2,26 +2,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Transform camera;
-    public float speed = 2;
+    public new Transform camera;
+    public float speed = 6;
+    public Rigidbody rb;
 
 
     private void Start()
     {
 
     }
-
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            print("space");
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            print("A");
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-            
-        }
+        Transform camTransform = Camera.main.transform;
+        Vector3 camPosition = new Vector3(camTransform.position.x, transform.position.y, camTransform.position.z);
+        Vector3 direction = (transform.position - camPosition).normalized;
+        Vector3 forwardMovement = direction * Input.GetAxis("Vertical");
+        Vector3 horizontalMovement = camTransform.right * Input.GetAxis("Horizontal");
+        Vector3 movement = Vector3.ClampMagnitude(forwardMovement + horizontalMovement, 1);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity += Physics.gravity * Time.fixedDeltaTime; // In PhysX, Acceleration ignores mass
+        float rigidbodyDrag = Mathf.Clamp01(1.0f - (rb.drag * Time.fixedDeltaTime));
+        rb.velocity *= rigidbodyDrag;
+        transform.position += rb.velocity * Time.fixedDeltaTime;
     }
 }
