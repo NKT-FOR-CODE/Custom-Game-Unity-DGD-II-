88b728c7542c481cc2de5840370212484fc151f7
WillysWrath <124813277+WillysWrath@users.noreply.github.com>
2023-04-21 09:42:55 -0700

    map got even better

---
+++ Custom Game Unity/Assets/RestartLevel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RestartLevel : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("RestartLevel"))
+        {
+            SceneManager.LoadScene("game dev project 1");
+        }
+
+    }
+}
