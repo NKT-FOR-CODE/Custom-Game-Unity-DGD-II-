4c4c23bb8c5f9860cd77c57df13f88ae6ab4cd70
WillysWrath <124813277+WillysWrath@users.noreply.github.com>
2023-04-21 10:14:46 -0700

    finished map

--- Custom Game Unity/Assets/Personal made Scripts/RestartLevel.cs
+++ Custom Game Unity/Assets/Personal made Scripts/RestartLevel.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Restart : MonoBehaviour
+public class RestartLevel : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Restart"))
+        if (collision.gameObject.CompareTag("RestartLevel"))
         {
             SceneManager.LoadScene("game dev project 1");
         }
